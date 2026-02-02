17f5c809777849dfe30d18d4b710ac578aaa2c75
CodeManBist <sagarbist372@gmail.com>
2026-02-02 22:54:52 +0530

    backend done and started which frontend have completed login, register

---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>404 Not Found</h1>
+      <p>The Page you're looking doesn't work</p>
+    </div>
+  )
+}
+
+export default NotFound
